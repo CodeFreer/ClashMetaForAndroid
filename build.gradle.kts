5bc2162df015d14306239fd1c5205732d0a09837
Howard Cheung <yywsyywsyyws@gmail.com>
2022-09-04 20:29:37 +0800

    Chore: bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ subprojects {
             minSdk = 21
             targetSdk = 31
 
-            versionName = "2.5.11-pre02"
+            versionName = "2.5.11-pre03"
             versionCode = 205011
 
             resValue("string", "release_name", "v$versionName")
